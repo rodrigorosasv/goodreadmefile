84af13363542742772c49ba9aa404c356c97ecac
Rodrigo Rova <rodrigo.rosasv@gmail.com>
2020-06-11 17:20:58 -0500

    Running application

---
+++ README.md
@@ -0,0 +1,101 @@
+# WikiDrinks 
+![badge](https://camo.githubusercontent.com/5536e335426d79f50ee2d88b57d6c108074992d7/68747470733a2f2f696d672e736869656c64732e696f2f707970692f707976657273696f6e732f7974326d70332e737667)![license](https://camo.githubusercontent.com/6e97fc42cf3a7104864fabeae2596615886e6736/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f6c6963656e73652d4147504c2d626c75652e737667)   
+
+## Description
+Wikidrinks is a site where people can search the recipee to prepare his favorite drinks.
+
+Also people can find new Drinks to prepare thanks to the different filters in the site.
+
+-Search for a specific drink
+
+-Select a specific ingredient or a glass type and get suggestions.
+
+-Select a category and get drink suggestions
+
+Have fun reading the instructions, everything is animated with gifs.
+
+People can also have a good read when having his/her favorite drink, Wikidrinks gives you reading recommendations.
+
+
+
+## Table of contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+* [Github info](#Github)
+
+## Installation
+1. Download the files
+
+2. Extract the files
+
+3. Open index.html
+
+
+
+## Usage 
+1. Open index.html.
+
+2. Navigate the site.
+
+3. Search for a specific drink.
+
+4. Or get suggested Drinks.
+
+
+## Contributing
+This is a code free to share.
+
+
+
+
+## Tests
+` const Arithmetic = require("../arithmetic");
+
+describe("Arithmetic", () => {
+  describe("Initialization", () => {
+    it("should return an object containing a 'number' property when called with the 'new' keyword", () => {
+      const obj = new Arithmetic();
+
+      expect("number" in obj).toEqual(true);
+    });
+
+    it("should set 'number' when created", () => {
+      const num = 108;
+
+      const obj = new Arithmetic(num);
+
+      expect(obj.number).toEqual(num);
+    });
+
+    it("should default 'number' to 0", () => {
+      const obj = new Arithmetic();
+
+      expect(obj.number).toEqual(0);
+    });
+  });`
+
+
+## Questions
+-What can I expect from this site?
+
+Get drinks recipees and interesting articles to read.
+
+-What can you find in this site?
+
+Step by step drink recipees.
+
+Options to find drinks.
+
+Suggested drinks options.
+
+
+## Github
+### Picture
+![githubPicture](https://avatars3.githubusercontent.com/u/60230421?s=400&u=f6fff7b943fa1979272b8fd4a45928f9be5e2d26&v=4)
+
+### Github Email
+<rodrigo.rosasv@gmail.com>
+
---
+++ index.js
@@ -0,0 +1,118 @@
+const fs = require("fs");
+const inquirer = require("inquirer");
+console.log("In the editors you can use Markup language")
+inquirer
+  .prompt([
+    {
+        type: "input",
+        message: "Writte the project title:",
+        name: "projectTitle"
+      },
+      {
+        type: "editor",
+        message: "Add a descripton:",
+        name: "description"
+      },
+    {
+        type: "editor",
+        message: "Writte the installation steps:",
+        name: "installation"
+    },
+    {
+        type: "input",
+        message: "Writte the URL of your Badge:",
+        name: "badge"
+    },
+    {
+        type: "editor",
+        message: "Writte how the application must be used:",
+        name: "usage"
+    },
+    {
+        type: "input",
+        message: "Writte the URL of your License:",
+        name: "license"
+    },
+    {
+        type: "editor",
+        message: "Writte the guidelines for contributors:",
+        name: "contributing"
+    },
+    {
+        type: "editor",
+        message: "Writte the test you have done for your application:",
+        name: "tests"
+    },
+    {
+        type: "editor",
+        message: "Questions",
+        name: "questions"
+    },
+    {
+        type: "input",
+        message: "Writte the URL of your GitHub profile picture: ",
+        name: "picture"
+    },
+    {
+        type: "input",
+        message: "Writte your GitHub user email:",
+        name: "email"
+    }
+  ])
+.then(function(response){
+    badgesNum=response.badge;
+    let readMeContent= generateReadme(response);
+    fs.writeFile("README.md",readMeContent, function(err) {
+
+        if (err) {
+          console.log(err);
+        }
+        else {
+          console.log("Commit logged!");
+        }
+      
+      });
+});
+
+function generateReadme(response){
+    
+    let readmeStructure=
+    `# ${response.projectTitle} 
+![badge](${response.badge})![license](${response.license})   
+
+## Description
+${response.description}
+
+## Table of contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+* [Github info](#Github)
+
+## Installation
+${response.installation}
+
+## Usage 
+${response.usage}
+
+## Contributing
+${response.contributing}
+
+## Tests
+${response.tests}
+
+## Questions
+${response.questions}
+
+## Github
+### Picture
+![githubPicture](${response.picture})
+
+### Github Email
+<${response.email}>
+
+`;  
+  return readmeStructure
+}
